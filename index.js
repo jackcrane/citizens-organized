ef81db624d4233d3a57c337dcb6500772058682e
Jack Crane <jack@jackcrane.rocks>
2024-08-09 16:43:56 -0400

    Forgot next()

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use((req, res, next) => {
   console.log(
     `${req.method} ${req.url} - ${req.ip} - ${new Date().toLocaleString()}`,
   );
+  next();
 });
 
 app.use(express.static("static"));
