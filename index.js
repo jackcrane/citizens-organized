c193756341ed00db16dd985a40f81ad1b0045f51
Jack Crane <jack@jackcrane.rocks>
2024-08-09 16:41:16 -0400

    Redirect to correct domain, and add logging

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ import express from "express";
 const app = express();
 app.set("view engine", "ejs");
 
+app.use((req, res, next) => {
+  console.log(
+    `${req.method} ${req.url} - ${req.ip} - ${new Date().toLocaleString()}`,
+  );
+});
+
 app.use(express.static("static"));
+
+// If the user has hit this with the domain "citizensorganize.com", redirect them to "citizensorganized.com".
+app.use((req, res, next) => {
+  if (req.hostname === "citizensorganize.com") {
+    return res.redirect(301, "http://citizensorganized.com" + req.originalUrl);
+  }
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("index");
